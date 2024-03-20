1739295520156ce36aa633c625a2d00f8e13b006
johnny <john_earl_dunbar@protonmail.com>
2024-03-19 22:41:07 -0600

    :)

--- src/headers/instrument_properties.cpp
+++ src/headers/instrument_properties.cpp
@@ -50,7 +50,7 @@ void Instrument_properties::update()
 
     refresh();
 }
-//test merge
+//test merge2
 void Instrument_properties::refresh()
 {
     SDL_SetRenderDrawColor(render, 128, 128, 128, 0xFF); // Background color
